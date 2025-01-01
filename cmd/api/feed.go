208b289ecc7c815a72b0a80dbfdaa0d368c706c7
mujeebcodes <mailmujeeb@icloud.com>
2025-01-01 21:32:31 +0000

    implemented filtering, sorting and pagination

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,11 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"github.com/mujeebcodes/go-social/internal/store"
+	"net/http"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(150))
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(150), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
